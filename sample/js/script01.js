a5bde9baf64fea73b70a672f94132d29e2cfef6b
shiotachiki <107100624+shiotachiki@users.noreply.github.com>
2022-11-04 16:12:47 +0900

    change

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -33,4 +33,10 @@ $(function(){
     $("#food").addClass("anime")
   }
 
-})
+  $(document).ready(function() {
+    var $magic = $(".magic"),
+        magicWHalf = $magic.width() / 2;
+    $(document).on("mousemove", function(e) {
+      $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
+    });
+  });
